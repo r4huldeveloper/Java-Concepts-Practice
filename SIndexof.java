a1eab9409c59ef0e60767115174bcac6117dc8d1
Rahul Sharma <developerrahul671@gmail.com>
2023-04-23 22:43:19 +0530

    Add files via upload

---
+++ SIndexof.java
@@ -0,0 +1,14 @@
+public class SIndexof{
+ public static void main(String[] args){
+   String str = "my name is rahul";
+
+// passing substring
+     System.out.println(str.indexOf("rahul"));
+     System.out.println();
+
+// passing substring with from index
+   String str1 = "you are are are good"  ;
+System.out.println(str1.indexOf("are",9));
+
+}
+}
