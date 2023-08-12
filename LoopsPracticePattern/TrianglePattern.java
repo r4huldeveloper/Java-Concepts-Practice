4a4a7abc9e0fcb95e8882e8cd560467bca38797a
Rahul Sharma <developerrahul671@gmail.com>
2023-08-12 21:58:26 +0530

    Update TrianglePattern.java

--- LoopsPracticePattern/TrianglePattern.java
+++ LoopsPracticePattern/TrianglePattern.java
@@ -19,3 +19,33 @@ public class TrianglePattern {
         sc.close();
     }
 }
+
+----------------------------------------
+With While-Loop
+
+    import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        /*
+        1 
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+        */
+        int i = 1;
+        while(i <= n){
+            int j = 1;
+            while(j <= i){
+                System.out.print(j);
+                j++;
+            }
+             System.out.println();
+             i++;
+            }
+         sc.close();
+        }
+    }
+
